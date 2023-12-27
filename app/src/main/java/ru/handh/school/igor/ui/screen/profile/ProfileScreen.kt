6cb1f4d84d6090903164f26938516ee9ef3c4587
pankratov.aleksey <pankratov.aleksey@gmail.com>
2023-12-27 15:54:28 +0300

    refactor: some changes

--- app/src/main/java/ru/handh/school/igor/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/ru/handh/school/igor/ui/screen/profile/ProfileScreen.kt
@@ -6,14 +6,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -23,14 +34,14 @@ import ru.handh.school.igor.ui.navigation.NavigationItem
 import ru.handh.school.igor.ui.theme.AppTheme
 
 private var DefaultContainerHeight = 56.dp
-private var fontSize = 100.sp
+private var fontSize = 24.sp
 
 @Composable
 fun ProfileScreen(
     vm: ProfileViewModel, context: Context, navController: NavHostController
 ) {
     val state by vm.state.collectAsState()
-    ProfileContent(state = state, navController = navController, onAction = vm::onAction)
+    //ProfileContent(state = state, navController = navController, onAction = vm::onAction)
 
     LaunchedEffect(state.result) {
         when (state.result) {
@@ -42,11 +53,13 @@ fun ProfileScreen(
                     context, "Server didn't respond", Toast.LENGTH_LONG
                 ).show()
             }
+
             is ResultProfile.UnknownError -> {
                 Toast.makeText(
                     context, "Error's occurred", Toast.LENGTH_LONG
                 ).show()
             }
+
             is ResultProfile.LogOut -> {
                 navController.navigate(NavigationItem.SignIn.route)
             }
@@ -54,32 +67,71 @@ fun ProfileScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun ProfileContent(
-    state: ProfileState, navController: NavHostController, onAction: (ProfileAction) -> Unit = {},
+    //state: ProfileState, navController: NavHostController, onAction: (ProfileAction) -> Unit = {},
 ) {
-    Column(modifier = Modifier.padding((AppTheme.offsets.medium))) {
-        Text(
-            text = "Profile", fontSize = fontSize, modifier = Modifier.weight(1f)
-        )
-        Button(modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = AppTheme.offsets.large)
-            .height(DefaultContainerHeight),
-            content = { Text(text = stringResource(R.string.about)) },
-            shape = (AppTheme.roundings.large),
-            onClick = {
-                navController.navigate(NavigationItem.About.route)
-            })
-        Button(
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(
+                    text = stringResource(R.string.profile),
+                    fontSize = fontSize,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth()
+                )
+            },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Filled.Info, contentDescription = null)
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            bottom = AppTheme.offsets.large,
+                            start = AppTheme.offsets.medium,
+                            end = AppTheme.offsets.medium
+                        )
+                        .height(DefaultContainerHeight),
+                    content = { Text(text = stringResource(R.string.out)) },
+                    shape = (AppTheme.roundings.large),
+                    onClick = {
+                        //onAction(ProfileAction.SubmitClicked)
+                    })
+            }
+        }
+    ) { containerPadding ->
+
+        Column(
             modifier = Modifier
+                .padding(AppTheme.offsets.medium)
+                .padding(containerPadding)
+        )
+        {
+/*            Button(modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = AppTheme.offsets.large)
                 .height(DefaultContainerHeight),
-            content = { Text(text = stringResource(R.string.out)) },
-            shape = (AppTheme.roundings.large),
-            onClick = {
-                onAction(ProfileAction.SubmitClicked)
-            })
+                content = { Text(text = stringResource(R.string.about)) },
+                shape = (AppTheme.roundings.large),
+                onClick = {
+                    //navController.navigate(NavigationItem.About.route)
+                })*/
+
+        }
     }
 }
